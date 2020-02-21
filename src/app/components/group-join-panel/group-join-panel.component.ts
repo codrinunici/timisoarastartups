09877fdd9f678d1d41a5d84dbfe667a1896b2057
Mike D <mike@dolha.xyz>
2020-02-21 18:08:10 +0200

    Add rudimenatry facebook group join btn jumbo

--- src/app/components/group-join-panel/group-join-panel.component.ts
+++ src/app/components/group-join-panel/group-join-panel.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./group-join-panel.component.scss']
 })
 export class GroupJoinPanelComponent implements OnInit {
-
+  facebookGroupLink = 'https://www.facebook.com/groups/196454194891791/';
   constructor() { }
 
   ngOnInit(): void {
