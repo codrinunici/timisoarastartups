7a22f9fc484ae58bd51d234b5986df72eb9e6f56
Mike D <mike@dolha.xyz>
2020-04-25 15:31:45 +0300

    Make successful call to to newsletter signup

--- src/app/components/subscribe-jumbo/subscribe-jumbo.component.ts
+++ src/app/components/subscribe-jumbo/subscribe-jumbo.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../../api/api.service';
 
 @Component({
   selector: 'app-subscribe-jumbo',
   templateUrl: './subscribe-jumbo.component.html',
-  styleUrls: ['./subscribe-jumbo.component.scss']
+  styleUrls: ['./subscribe-jumbo.component.scss'],
 })
-export class SubscribeJumboComponent implements OnInit {
+export class SubscribeJumboComponent {
+  newsletterForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private api: ApiService, private fb: FormBuilder) {
+    this.newsletterForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+    });
   }
 
+  submitForm() {
+    this.api.newsletterSignup(this.newsletterForm.value).subscribe();
+  }
 }
