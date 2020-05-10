fa1ced92d32284fb1aa4eb8c37d58476cd82b0ea
Mike D <mike@dolha.xyz>
2020-05-10 13:51:36 +0300

    Add people page

--- src/app/pages/people/people.component.ts
+++ src/app/pages/people/people.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  faAngellist,
+  faAppStoreIos,
+  faFacebookSquare,
+  faGooglePlay,
+  faLinkedin,
+  faTwitterSquare
+} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare, faLink } from '@fortawesome/free-solid-svg-icons';
+import { Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
+
+import { ApiService } from './../../api/api.service';
+
 
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
-  styleUrls: ['./people.component.scss']
+  styleUrls: ['./people.component.scss'],
+  providers: [ApiService]
 })
-export class PeopleComponent implements OnInit {
+export class PeopleComponent implements OnInit, OnDestroy {
+  destroy$: Subject<boolean>;
 
-  constructor() { }
+  people = [];
+  icon = {
+    angellist: faAngellist,
+    crunchbase: 'cb',
+    facebook: faFacebookSquare,
+    twitter: faTwitterSquare,
+    linkedin: faLinkedin,
+    website: faLink,
+    email: faEnvelopeSquare,
+    gplay: faGooglePlay,
+    appstore: faAppStoreIos,
+  };
+
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject();
+    this.api
+      .getPeople()
+      .pipe(
+        takeUntil(this.destroy$),
+        map(data => data.docs.map(doc => doc.data())),
+        map(docs =>
+          docs.sort((a, b) =>
+            a.name.toLocaleLowerCase().localeCompare(b.name.toLocaleLowerCase())
+          )
+        )
+      )
+      .subscribe(data => this.people = data)
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
 }
--- src/app/pages/people/people.module.ts
+++ src/app/pages/people/people.module.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { PeopleRoutingModule } from './people-routing.module';
 import { PeopleComponent } from './people.component';
 
 @NgModule({
   declarations: [PeopleComponent],
-  imports: [CommonModule, PeopleRoutingModule],
+  imports: [
+    CommonModule,
+    PeopleRoutingModule,
+    FontAwesomeModule,
+  ],
 })
-export class PeopleModule {}
+export class PeopleModule { }
