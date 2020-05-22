80dc20ae7154152e87f3cbcc826cd4ac9ba0a817
Mike D <mike@dolha.xyz>
2020-05-22 21:43:43 +0300

    Add article component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MarkdownModule } from 'ngx-markdown';
 
+import { ArticleComponent } from './article/article.component';
 import { GroupJoinPanelComponent } from './group-join-panel/group-join-panel.component';
 import { HeroCarouselComponent } from './hero-carousel/hero-carousel.component';
 import { SmallCarouselComponent } from './small-carousel/small-carousel.component';
@@ -19,12 +19,12 @@ import { TextSloganPanelComponent } from './text-slogan-panel/text-slogan-panel.
     SubscribeJumboComponent,
     TestimonialsPanelComponent,
     TextSloganPanelComponent,
+    ArticleComponent,
   ],
   imports: [
     CommonModule,
     NgbModule,
     ReactiveFormsModule,
-    MarkdownModule.forChild()
   ],
   exports: [
     GroupJoinPanelComponent,
@@ -33,6 +33,7 @@ import { TextSloganPanelComponent } from './text-slogan-panel/text-slogan-panel.
     SubscribeJumboComponent,
     TestimonialsPanelComponent,
     TextSloganPanelComponent,
+    ArticleComponent
   ],
 })
 export class ComponentsModule { }
