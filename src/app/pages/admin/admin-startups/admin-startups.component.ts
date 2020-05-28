6f81799bf7ecbd87ea9d1f20e08a3bb4f3944b5b
Mike D <mike@dolha.xyz>
2020-05-28 19:00:04 +0300

    Start rendering startups

--- src/app/pages/admin/admin-startups/admin-startups.component.ts
+++ src/app/pages/admin/admin-startups/admin-startups.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import Startup from '../../../models/startup';
+
+import { StartupsService } from './../../../api/startups/startups.service';
 
 @Component({
   selector: 'app-admin-startups',
   templateUrl: './admin-startups.component.html',
   styleUrls: ['./admin-startups.component.scss']
 })
-export class AdminStartupsComponent implements OnInit {
+export class AdminStartupsComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<boolean>;
 
-  constructor() { }
+  initialStartups: Startup[];
+  startups: Startup[];
+  startupForms: FormGroup;
+
+  constructor(
+    private startupsService: StartupsService,
+    private fb: FormBuilder
+  ) {
+    this.initialStartups = [];
+    this.startups = [];
+    this.startupForms = this.fb.group({
+      startups: this.fb.array([])
+    });
+  }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
+
+    this.startupsService.getStartups()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((startups: Startup[]) => this.initializeStartups(startups));
+  }
+
+  private initializeStartups(startups: Startup[]) {
+    const startupFormArray = this.startupForms.get('startups') as FormArray;
+    this.startups = [...startups];
+    this.initialStartups = [...startups];
+    this.startups.forEach(startup => {
+      startupFormArray.push(startup.buildReactiveFormGroup())
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
 }
