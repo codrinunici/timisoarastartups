1754c911c7d358c2188ffff3973104808a3cd432
Mike D <mike@dolha.xyz>
2020-05-30 19:44:10 +0300

    Add ability to delete startup

--- src/app/pages/admin/admin-startups/admin-startup-form/admin-startup-form.component.ts
+++ src/app/pages/admin/admin-startups/admin-startup-form/admin-startup-form.component.ts
@@ -1,16 +1,22 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import Startup from '../../../../models/startup';
 
+import { ModalConfirmationComponent } from './../../../../utils/modal-confirmation/modal-confirmation.component';
 import { StartupsService } from './../../../../api/startups/startups.service';
 
 @Component({
   selector: 'app-admin-startup-form',
   templateUrl: './admin-startup-form.component.html',
-  styleUrls: ['./admin-startup-form.component.scss']
+  styleUrls: ['./admin-startup-form.component.scss'],
+  providers: [NgbModal]
 })
-export class AdminStartupFormComponent implements OnInit {
+export class AdminStartupFormComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<boolean>;
   @Input() startupData: Startup;
   @Output() edited = new EventEmitter<Startup>();
   @Output() deleted = new EventEmitter<Startup>();
@@ -19,12 +25,15 @@ export class AdminStartupFormComponent implements OnInit {
 
   constructor(
     private startupsService: StartupsService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private modalService: NgbModal
   ) {
     this.startupForm = this.fb.group({});
   }
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
+
     this.setupForm(this.startupData);
   }
 
@@ -32,6 +41,7 @@ export class AdminStartupFormComponent implements OnInit {
     const formValue = new Startup(this.startupForm.value);
 
     this.startupsService.editStartup(formValue)
+      .pipe(takeUntil(this.destroy$))
       .subscribe(savedStartup => {
         this.setupForm(savedStartup);
         this.edited.emit(savedStartup);
@@ -41,7 +51,23 @@ export class AdminStartupFormComponent implements OnInit {
   remove() {
     const formValue = new Startup(this.startupForm.value);
 
-    this.deleted.emit(formValue);
+    const modalRef = this.modalService.open(ModalConfirmationComponent);
+    modalRef.componentInstance.headerTitle = 'Are you sure?';
+    modalRef.componentInstance.bodyDescription = 'Deleting a startup cannot be undone!';
+    modalRef.componentInstance.acceptBtn = 'Yes';
+    modalRef.componentInstance.cancelBtn = 'Cancel';
+
+    modalRef.result
+      .then(accepted => {
+        if (accepted) {
+          this.startupsService.removeStartup(formValue)
+            .pipe(takeUntil(this.destroy$))
+            .subscribe(removedStartup => this.deleted.emit(removedStartup));
+        }
+        return;
+      })
+      .catch(() => { });
+
   }
 
   enableEditing() {
@@ -54,6 +80,11 @@ export class AdminStartupFormComponent implements OnInit {
 
   isEnabled() {
     return this.startupForm.enabled;
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
   }
 
   private setupForm(startup: Startup) {
